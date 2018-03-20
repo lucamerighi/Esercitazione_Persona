9030f1bd42af3133d3c867ed00c9e90314d6561b
lucam <lucam@DESKTOP-C8OJ5N8>
2018-03-20 16:10:03 +0100

    Added documentation

--- Esercitazione_Persona/src/Persona.java
+++ Esercitazione_Persona/src/Persona.java
@@ -1,67 +1,81 @@
 
 public class Persona {
 	private String nameSurname;
-	private int yearOfBirth;	
-	
-	
+	private int yearOfBirth;
+
 	/**
 	 * Constructor for Person
-	 * @param Name and surname
-	 * @param Year of birth
+	 * 
+	 * @param Name
+	 *            and surname
+	 * @param Year
+	 *            of birth
 	 */
 	public Persona(String nameSurname, int yearOfBirth) {
 		super();
 		this.nameSurname = nameSurname;
 		this.yearOfBirth = yearOfBirth;
 	}
-	
+
 	/**
 	 * Constructor for Person with year as a string
-	 * @param Name and surname
-	 * @param Year of birth
+	 * 
+	 * @param Name
+	 *            and surname
+	 * @param Year
+	 *            of birth
 	 */
 	public Persona(String nameSurname, String yearOfBirth) {
 		this(nameSurname, Integer.parseInt(yearOfBirth));
-	}	
-	
+	}
+
 	/**
 	 * Getter for name and surname
+	 * 
 	 * @return Name and surname
 	 */
 	public String getNameSurname() {
 		return nameSurname;
 	}
-	
+
 	/**
 	 * Getter for year of birth
+	 * 
 	 * @return Year of birth
 	 */
 	public int getYearOfBirth() {
 		return yearOfBirth;
 	}
-	
+
 	/**
 	 * Check the names of 2 persons
-	 * @param Another person
+	 * 
+	 * @param Another
+	 *            person
 	 * @return True if both persons have the same name, false otherwise
 	 */
 	public boolean omonimo(Persona p) {
 		return this.nameSurname.equalsIgnoreCase(p.getNameSurname());
 	}
-	
+
 	/**
 	 * Checks which one of 2 persons is older
-	 * @param Another person
-	 * @return 1 if the current Person is younger, 0 if both have the same year of birth, -1 if the current person is older
+	 * 
+	 * @param Another
+	 *            person
+	 * @return 1 if the current Person is younger, 0 if both have the same year of
+	 *         birth, -1 if the current person is older
 	 */
 	public int isOlder(Persona that) {
-		int result = this.getYearOfBirth() - that.getYearOfBirth();		
+		int result = this.getYearOfBirth() - that.getYearOfBirth();
 		return (result < 0 ? -1 : (result == 0) ? 0 : 1);
 	}
-	
+
 	/**
 	 * Checks if the current person is older than everyone in the array
-	 * @param An array of persons
+	 * 
+	 * @param An
+	 *            array of persons
 	 * @return True if oldest, false otherwise
 	 */
 	public boolean olderThan(Persona[] others) {
@@ -76,7 +90,14 @@ public class Persona {
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Returns the oldest person in an array
+	 * 
+	 * @param Array
+	 *            of persons
+	 * @return The oldest person if there is one, or null
+	 */
 	public static Persona oldest(Persona[] persone) {
 		for (Persona p : persone) {
 			if (p.olderThan(persone)) {
@@ -85,11 +106,5 @@ public class Persona {
 		}
 		return null;
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
