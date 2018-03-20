7b0caa41729e5fb0c85900850266e85603e0a458
lucam <lucam@DESKTOP-C8OJ5N8>
2018-03-20 16:08:22 +0100

    Added methods

--- Esercitazione_Persona/src/Persona.java
+++ Esercitazione_Persona/src/Persona.java
@@ -1,4 +1,95 @@
 
 public class Persona {
+	private String nameSurname;
+	private int yearOfBirth;	
+	
+	
+	/**
+	 * Constructor for Person
+	 * @param Name and surname
+	 * @param Year of birth
+	 */
+	public Persona(String nameSurname, int yearOfBirth) {
+		super();
+		this.nameSurname = nameSurname;
+		this.yearOfBirth = yearOfBirth;
+	}
+	
+	/**
+	 * Constructor for Person with year as a string
+	 * @param Name and surname
+	 * @param Year of birth
+	 */
+	public Persona(String nameSurname, String yearOfBirth) {
+		this(nameSurname, Integer.parseInt(yearOfBirth));
+	}	
+	
+	/**
+	 * Getter for name and surname
+	 * @return Name and surname
+	 */
+	public String getNameSurname() {
+		return nameSurname;
+	}
+	
+	/**
+	 * Getter for year of birth
+	 * @return Year of birth
+	 */
+	public int getYearOfBirth() {
+		return yearOfBirth;
+	}
+	
+	/**
+	 * Check the names of 2 persons
+	 * @param Another person
+	 * @return True if both persons have the same name, false otherwise
+	 */
+	public boolean omonimo(Persona p) {
+		return this.nameSurname.equalsIgnoreCase(p.getNameSurname());
+	}
+	
+	/**
+	 * Checks which one of 2 persons is older
+	 * @param Another person
+	 * @return 1 if the current Person is younger, 0 if both have the same year of birth, -1 if the current person is older
+	 */
+	public int isOlder(Persona that) {
+		int result = this.getYearOfBirth() - that.getYearOfBirth();		
+		return (result < 0 ? -1 : (result == 0) ? 0 : 1);
+	}
+	
+	/**
+	 * Checks if the current person is older than everyone in the array
+	 * @param An array of persons
+	 * @return True if oldest, false otherwise
+	 */
+	public boolean olderThan(Persona[] others) {
+		for (Persona p : others) {
+			if (this.isOlder(p) > 0) {
+				return false;
+			} else if (this.isOlder(p) == 0) {
+				if (!this.nameSurname.equals(p.getNameSurname())) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static Persona oldest(Persona[] persone) {
+		for (Persona p : persone) {
+			if (p.olderThan(persone)) {
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
